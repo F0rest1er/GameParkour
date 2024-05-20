3b6ab87b95f7b1e13db6b32af082f1d9f734768a
F0rest1er <Romans1999@yandex.ru>
2024-05-20 17:37:47 +0300

    1

--- Assets/Scripts/DeathFloor.cs
+++ Assets/Scripts/DeathFloor.cs
@@ -3,8 +3,19 @@ using UnityEngine;
 public class DeathFloor : MonoBehaviour
 {
     public Vector3 Teleport_Point;
-    private void OnTriggerStay(Collider other)
+
+    private void OnCollisionEnter(Collision collision)
     {
-        other.transform.position = Teleport_Point;
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>(); // Получаем Rigidbody у объекта
+        CapsuleCollider cc = collision.gameObject.GetComponent<CapsuleCollider>(); // Получаем CapsuleCollider у объекта
+
+        if (rb != null && cc != null) // Проверяем наличие Rigidbody и CapsuleCollider
+        {
+            rb.velocity = Vector3.zero; // Обнуляем скорость объекта
+            rb.angularVelocity = Vector3.zero; // Обнуляем угловую скорость объекта
+
+            Vector3 newPos = new Vector3(Teleport_Point.x, Teleport_Point.y + cc.height / 2, Teleport_Point.z); // Учитываем высоту CapsuleCollider
+            collision.gameObject.transform.position = newPos; // Перемещаем объект на новую позицию
+        }
     }
 }
